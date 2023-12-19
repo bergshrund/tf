0b35f9b79aba0ce7d3a85872a6e3ecb23cfb7dc6
Andrew Ivanov <bergshrund@#gmail.com>
2023-12-19 14:41:37 +0200

    Initial configuration

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "GOOGLE_REGION" {
+  description = ""
+  type        = string
+}
+
+variable "GOOGLE_PROJECT" {
+  description = ""
+  type        = string
+ }
+
+variable "GKE_NUM_NODES" {
+  description = ""
+  type        = string
+}
