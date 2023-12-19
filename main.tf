c5c797bc1cd7d5dfbd7ca60cccf962ff7e156810
Andrew Ivanov <bergshrund@#gmail.com>
2023-12-19 14:48:27 +0200

    Added new vars

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 module "gke_cluster" {
   source         = "github.com/bergshrund/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GKE_CLUSTER_NAME = var.GKE_CLUSTER_NAME
+  GKE_MACHINE_TYPE = var.GKE_MACHINE_TYPE
   GKE_NUM_NODES  = var.GKE_NUM_NODES
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,24 @@
-variable "GOOGLE_REGION" {
-  description = ""
+variable "GOOGLE_PROJECT" {
   type        = string
+  description = "GCP project name"
 }
 
-variable "GOOGLE_PROJECT" {
-  description = ""
+variable "GOOGLE_REGION" {
   type        = string
- }
+  description = "GCP region to use"
+}
+
+variable "GKE_MACHINE_TYPE" {
+  type        = string
+  description = "Machine type"
+}
 
 variable "GKE_NUM_NODES" {
-  description = ""
+  type        = number
+  description = "GKE nodes number"
+}
+
+variable "GKE_CLUSTER_NAME" {
   type        = string
+  description = "GKE cluster name"
 }
